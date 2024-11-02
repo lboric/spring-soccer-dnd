9eed816b9984c70c7cb056c3e93b2e974d34e47c
lboric <luka.boric10@gmail.com>
2024-11-02 23:59:28 +0100

    LBSD-001: Project init

---
+++ src/main/java/com/soccerdnd/spring_soccer_dnd/SpringSoccerDndApplication.java
@@ -0,0 +1,13 @@
+package com.soccerdnd.spring_soccer_dnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoccerDndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoccerDndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soccerdnd/spring_soccer_dnd/SpringSoccerDndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soccerdnd.spring_soccer_dnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoccerDndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
